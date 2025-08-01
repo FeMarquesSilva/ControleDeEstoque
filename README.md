631281c94094498cf082cd47268adf275162e159
FeMarquesSilva <felipemarques330@gmail.com>
2025-08-01 19:54:41 -0300

    Adicionando botões perssonalizados na tela de home

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 Projeto desenvolvido na PUC para a Coamo - Projeto de Desenvolvimento de Software.
 
 ## Tecnologias Utilizadas ##
-* Backend:
-- Python 3.13.3
+*# Backend:
+- Python 3.12.0
 - Flask 2.3.3
 - SQLAlchemy 2.0.23
 - python-dotenv 1.0.0
 
-* Frontend:
-- Chakra Ui 
+*# Frontend:
+- Chakra-Ui 
+- React
+- React-Router-Dom
 
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,37 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Text } from "@chakra-ui/react";
+
 
 const Home = () => {
+
+    const sections = ["Produtos", "Clientes", "Fornecedores"];
+    
     return (
-        <Box>
-            <Flex justifyContent="center" alignItems="center" height="100vh">
-                <Text fontSize="2xl">Pagina Inicial a ser desenvolvida!</Text>
-            </Flex>
-        </Box>
+        <Flex 
+        flexDir={"column"} 
+        backgroundColor={"rgba(255, 226, 171, 1)"} 
+        w={"100%"} h={"100vh"} 
+        display={"flex"} 
+        alignItems={"center"} 
+        justifyContent={"center"} 
+        gap={"20px"}>
+
+            {sections.map((info) => {
+                return (
+                    <Box 
+                    backgroundColor={"rgba(223, 152, 0, 1)"} 
+                    w={"150px"} textAlign={"center"} p={"15px"} 
+                    borderRadius={"15px"} 
+                    _hover={{ backgroundColor: "rgba(200, 130, 0, 1)", padding: "20px", width: "170px", cursor: "pointer" }} 
+                    key={info}
+                    transition={"all 0.3s"}>
+
+                        <Text>{info}</Text>
+
+                    </Box>
+
+                );
+                })}
+        </Flex>
     );
 }
 
