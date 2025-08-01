5cfd9b9856b9d4b2b019e9c1b9e353f3a686c285
FeMarquesSilva <felipemarques330@gmail.com>
2025-07-31 21:17:10 -0300

    Adicionando componentes do chakra ui e ajustando a estrutura do index.tsx com provider do chakra para os routeamentos

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import { Provider } from './components/ui/provider';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
+
