e29170c745294c3b7531a503a92acfc1d2560dbd
FeMarquesSilva <felipemarques330@gmail.com>
2025-07-31 21:29:20 -0300

    Ajustando erros do arquivo de componentes do chakra ui e organizando o a app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { Toaster } from "./components/ui/toaster";
-import React, { useEffect } from 'react';
+import React from 'react';
 
 //Importando o componentes do sistema:
 import Login from './pages/Login';
 
+// Definindo as rotas do aplicativo:
 const router = createBrowserRouter([
   { path: "/", element: <Login /> },
 ]);
-
 
 const App: React.FC = () => {
   return (
