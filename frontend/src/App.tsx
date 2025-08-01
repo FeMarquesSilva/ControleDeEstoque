4f13cc41af30aa3c2a0829b6019921887f736e99
FeMarquesSilva <felipemarques330@gmail.com>
2025-07-31 21:22:03 -0300

    Adicionando o router dom e ajustando o arquivo app do front com as configurações inicias para fazer o routeamento

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,53 +1,23 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { Toaster } from "./components/ui/toaster";
 import React, { useEffect } from 'react';
 
-interface Cliente {
-  id: number;
-  nome: string;
-  email: string;
-  telefone: string;
-}
+//Importando o componentes do sistema:
+import Login from './pages/Login';
 
-function App() {
-  const [clientes, setClientes] = React.useState<Cliente[]>([]);
+const router = createBrowserRouter([
+  { path: "/", element: <Login /> },
+]);
 
-  const buscarClientes = async () => {
-    try {
-      const response = await fetch('http://localhost:5000/clientes');
-      if (!response.ok) {
-        throw new Error('Erro ao buscar clientes');
-      }
-      const data = await response.json();
-      setClientes(data); // ✅ Aqui atualiza o estado
-    } catch (error) {
-      console.error('Erro:', error);
-    }
-  }
 
-  useEffect(() => {
-    buscarClientes();
-  }, []);
-
+const App: React.FC = () => {
   return (
-    <div>
-      <header>
-        <h1>Base do projeto</h1>
-      </header>
-
-      {clientes.length > 0 ? (
-        <ul>
-          {clientes.map(cliente => (
-            <li key={cliente.id}>
-              {cliente.nome} - {cliente.email} - {cliente.telefone}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>Não há clientes cadastrados.</p>
-      )}
-
-      <button onClick={() => console.log(clientes)}>Ver dados no console</button>
-    </div>
+    <ChakraProvider value={defaultSystem}>
+      <Toaster />
+      <RouterProvider router={router} />
+    </ChakraProvider>
   );
-}
+};
 
 export default App;
