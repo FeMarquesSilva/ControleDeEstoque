56dd758baa26ad893d028666e0addb277c892e87
Anderson-dev999 <ander.taylor1@gmail.com>
2025-08-02 11:07:24 -0300

    Criado botão de fornecedor

---
+++ frontend/src/pages/RoutesFornecedor/AdicionarForncedor.tsx
@@ -0,0 +1,15 @@
+import { Box } from "@chakra-ui/react";
+import Header from "../../components/ui/Header";
+import BTReturn from "../../components/ui/BTReturn";
+
+const AdicionarFornecedor = () => {
+    return (
+        <Box>
+            <Header />
+            <BTReturn />
+            {/* Conteúdo da página de Adicionar Fornecedor */}
+        </Box>
+    );
+};
+
+export default AdicionarFornecedor;
