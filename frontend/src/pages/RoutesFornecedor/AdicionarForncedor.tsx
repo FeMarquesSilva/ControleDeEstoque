bbdd215a80ffcfda51f431a38c1ff689ab7f75d2
FeMarquesSilva <felipemarques330@gmail.com>
2025-08-05 19:49:05 -0300

    Criando os inputs do cadastro de fornecedores

--- frontend/src/pages/RoutesFornecedor/AdicionarForncedor.tsx
+++ frontend/src/pages/RoutesFornecedor/AdicionarForncedor.tsx
@@ -1,6 +1,13 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import Header from "../../components/ui/Header";
 import BTReturn from "../../components/ui/BTReturn";
+
+const stylesInputs = {
+    width: "100%",
+    padding: "5px",
+    borderRadius: "8px",
+    border: "1px solid #ccc",
+};
 
 const AdicionarFornecedor = () => {
     return (
@@ -8,15 +15,46 @@ const AdicionarFornecedor = () => {
             <Header tittle="Cadastrar Fornecedor" />
             <BTReturn />
 
-            { /* Componente do formulário para cadastro do produto */ }
+            { /* Componente do formulário para cadastro do produto */}
             <Box
                 display={"flex"}
                 justifyContent={"center"}
                 alignItems={"center"}
             >
-                
-                <Flex mt={"80px"} backgroundColor={"rgba(172, 132, 59, 1)"} w={"600px"} h={"400px"} flexDir={"column"} p={"20px"} borderRadius={"15px"}>
-                    
+
+                <Flex 
+                mt={"80px"} 
+                backgroundColor={"rgba(172, 132, 59, 1)"} 
+                flexDir={"column"} 
+                p={"15px"} 
+                borderRadius={"15px"}
+                gap={"5px"}>
+
+                    <Text fontSize={"20px"} fontWeight={"bold"} color={"white"}>
+                        Preencha os dados abaixo:
+                        </Text>
+
+                    <Box>
+                        <Text>Nome</Text>
+                        <input type={"text"} placeholder={"Nome do Fornecedor"} style={stylesInputs} />
+                    </Box>
+                    <Box>
+                        <Text>CNPJ</Text>
+                        <input type={"text"} placeholder={"CNPJ do Fornecedor"} style={stylesInputs} />
+                    </Box>
+                    <Box>
+                        <Text>Contato</Text>
+                        <input type={"text"} placeholder={"Contato do Fornecedor"} style={stylesInputs} />
+                    </Box>
+                    <Box>
+                        <Text>Endereço</Text>
+                        <input type={"text"} placeholder={"Endereço do Fornecedor"} style={stylesInputs} />
+                    </Box>
+                    <Box>
+                        <Text>Email</Text>
+                        <input type={"email"} placeholder={"Email do Fornecedor"} style={stylesInputs} />
+                    </Box>
+
                 </Flex>
 
             </Box>
