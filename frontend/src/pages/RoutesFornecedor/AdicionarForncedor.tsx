ba31970a85cffab110a5996be09627eaaa6b79ed
FeMarquesSilva <felipemarques330@gmail.com>
2025-08-05 21:18:48 -0300

    Ajustando as funcionalidades dos inputs, com a interface de fornecedor para ir armazenando os dados

--- frontend/src/pages/RoutesFornecedor/AdicionarForncedor.tsx
+++ frontend/src/pages/RoutesFornecedor/AdicionarForncedor.tsx
@@ -1,6 +1,8 @@
 import { Box, Button, Flex, Text } from "@chakra-ui/react";
 import Header from "../../components/ui/Header";
 import BTReturn from "../../components/ui/BTReturn";
+import { useState } from "react";
+import { Fornecedor } from "./Interfaces";
 
 const stylesInputs = {
     width: "100%",
@@ -10,6 +12,19 @@ const stylesInputs = {
 };
 
 const AdicionarFornecedor = () => {
+
+    const [fornecedor, setFornecedor] = useState<Fornecedor>({
+        nome: "",
+        cnpj: "",
+        contato: "",
+        endereco: "",
+        email: "",
+    })
+
+    const handleTeste = () => {
+        console.log(fornecedor);
+    }
+
     return (
         <Box>
             <Header tittle="Cadastrar Fornecedor" />
@@ -36,23 +51,28 @@ const AdicionarFornecedor = () => {
 
                     <Box>
                         <Text>Nome</Text>
-                        <input type={"text"} placeholder={"Nome do Fornecedor"} style={stylesInputs} />
+                        <input type={"text"} placeholder={"Nome do Fornecedor"} style={stylesInputs}
+                            onChange={(e) => setFornecedor({ ...fornecedor, nome: e.target.value })} />
                     </Box>
                     <Box>
                         <Text>CNPJ</Text>
-                        <input type={"text"} placeholder={"CNPJ do Fornecedor"} style={stylesInputs} />
+                        <input type={"text"} placeholder={"CNPJ do Fornecedor"} style={stylesInputs}
+                            onChange={(e) => setFornecedor({ ...fornecedor, cnpj: e.target.value })} />
                     </Box>
                     <Box>
                         <Text>Contato</Text>
-                        <input type={"text"} placeholder={"Contato do Fornecedor"} style={stylesInputs} />
+                        <input type={"text"} placeholder={"Contato do Fornecedor"} style={stylesInputs}
+                            onChange={(e) => setFornecedor({ ...fornecedor, contato: e.target.value })} />
                     </Box>
                     <Box>
                         <Text>Endereço</Text>
-                        <input type={"text"} placeholder={"Endereço do Fornecedor"} style={stylesInputs} />
+                        <input type={"text"} placeholder={"Endereço do Fornecedor"} style={stylesInputs}
+                            onChange={(e) => setFornecedor({ ...fornecedor, endereco: e.target.value })} />
                     </Box>
                     <Box>
                         <Text>Email</Text>
-                        <input type={"email"} placeholder={"Email do Fornecedor"} style={stylesInputs} />
+                        <input type={"email"} placeholder={"Email do Fornecedor"} style={stylesInputs}
+                            onChange={(e) => setFornecedor({ ...fornecedor, email: e.target.value })} />
                     </Box>
 
                     <Button
@@ -60,8 +80,9 @@ const AdicionarFornecedor = () => {
                         w={"100%"}
                         backgroundColor={"rgba(46, 126, 39, 1)"}
                         color={"white"}
-                        transition={"all 0.3s"} 
-                        _hover={{ backgroundColor: "rgba(85, 138, 80, 1)" }}>Salvar</Button>
+                        transition={"all 0.3s"}
+                        _hover={{ backgroundColor: "rgba(85, 138, 80, 1)" }}
+                        onClick={() => {handleTeste()}}>Salvar</Button>
                 </Flex>
             </Box>
         </Box>
