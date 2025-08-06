db2208d988c936092d96ed85f67faf769ccb4b86
FeMarquesSilva <felipemarques330@gmail.com>
2025-08-06 16:55:16 -0300

    Criando o serviço de get fornecedores

--- frontend/src/pages/RoutesFornecedor/ListarFornecedores.tsx
+++ frontend/src/pages/RoutesFornecedor/ListarFornecedores.tsx
@@ -1,16 +1,31 @@
 import { Box, Flex, Text } from "@chakra-ui/react";
 import Header from "../../components/ui/Header";
 import BTReturn from "../../components/ui/BTReturn";
+import { useEffect, useState } from "react";
+import { Fornecedor } from "./Interfaces";
+import { fetchFornecedores } from "./Services";
 
 const ListarFornecedores = () => {
 
+    const [ fornecedores, setFornecedores ] = useState<Fornecedor[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetchFornecedores();
+            if (response?.status === 200) {
+                setFornecedores(response.data);
+            } else {
+                alert("Erro ao buscar fornecedores. Tente novamente.");
+            }
+        };
+        fetchData();
+    }
+    , []);
 
     return (
         <Box>
             <Header tittle="Lista de Fornecedores" />
             <BTReturn />
-            {/* Conteúdo da página de Listar Fornecedores */}
-
 
             { /* Componente do formulário para cadastro do produto */}
             <Box
