b7f3bc64e9f47cb0a38ac5f7eb5ed8b6b53fc388
FeMarquesSilva <felipemarques330@gmail.com>
2025-08-09 16:58:22 -0300

    Adicionando setuptools para as configurações necessárias do pyrebase

--- backend/database/auth_firebase.py
+++ backend/database/auth_firebase.py
@@ -1,7 +1,11 @@
 import pyrebase
 from config.firebase import firebaseConfig
 
+print("Iniciando conexão com o Firebase...")
+print("Configuração do Firebase:", firebaseConfig)
+
 firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
 
-user = auth.create_user_with_email_and_password("teste@testes,com", "12345")
+user = auth.create_user_with_email_and_password("teste@testes.com", "123456")
+print("Usuário criado:", user)
