c67e971e4174affc3340fd997f56a925997daf66
Anderson-dev999 <ander.taylor1@gmail.com>
2025-08-14 17:22:27 -0300

    ajustando e finalizando a tela de listagem de produto

--- frontend/src/pages/RoutesProduto/ListarProduto.tsx
+++ frontend/src/pages/RoutesProduto/ListarProduto.tsx
@@ -1,11 +1,111 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button, Flex, Text } from "@chakra-ui/react";
+import Header from "../../components/ui/Header";
+import BTReturn from "../../components/ui/BTReturn";
+import { useEffect, useState } from "react";
+import { deleteProduto, fetchProdutos } from "./Services";
+import { useNavigate } from "react-router-dom";
+import { menssage } from "../../components/ui/toastMenssage";
+import { Produto } from "./Interface";
 
 const ListarProduto = () => {
+
+    const [produtos, setProdutos] = useState<Produto[]>([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetchProdutos();
+            if (response?.status === 200) {
+                setProdutos(response.data);
+            } else {
+                menssage("Erro", "Erro ao buscar produtos. Tente novamente.", "error");
+            }
+        };
+        fetchData();
+    }, []);
+
+    const handleDell = async (id: number) => {
+        if (window.confirm("Tem certeza que deseja excluir este produto?")) {
+            try {
+                const response = await deleteProduto(id);
+
+                if (response?.status === 200) {
+                    menssage("Sucesso", "Produto excluído com sucesso!", "success");
+                    setProdutos(produtos.filter(produto => produto.id !== id));
+                } else {
+                    menssage("Erro", "Erro ao excluir produto. Tente novamente.", "error");
+                }
+            } catch (error) {
+                console.error(error);
+                menssage("Erro", "Erro ao excluir produto. Tente novamente.", "error");
+            }
+        }
+    }
+
     return (
         <Box>
-            <h1>Produtos</h1>
+            <Header tittle="Lista de Produtos" />
+            <BTReturn />
+
+            <Box
+                display={"flex"}
+                justifyContent={"center"}
+                alignItems={"center"}
+            >
+                <Box mt={"50px"} border={"1px solid #ccc"} borderRadius="2px" textAlign={"center"}>
+                    {/* Cabeçalho */}
+                    <Flex backgroundColor={"rgba(146, 105, 29, 1)"} fontWeight="bold" p={3} borderBottom="1px solid #ccc" justifyContent="space-between" gap={"50px"}>
+                        <Box flex="1">ID</Box>
+                        <Box flex="2">Nome</Box>
+                        <Box flex="2">CNPJ</Box>
+                        <Box flex="2">Email</Box>
+                        <Box flex="2">Contato</Box>
+                        <Box flex="3">Endereço</Box>
+                        <Box flex="1.6">Ação</Box>
+                    </Flex>
+
+                    {/* Linhas de produtos */}
+                    {produtos.map((produto, index) => (
+                        <Flex
+                            key={produto.id}
+                            p={3}
+                            bg={index % 2 === 0 ? 'rgba(173, 142, 84, 1)' : 'rgba(126, 118, 103, 1)'}
+                            borderBottom="1px solid #eee"
+                            gap={"50px"}
+                            transition={'all 0.3s'}
+                            _hover={{ backgroundColor: 'rgba(250, 218, 158, 0.8)', cursor: 'pointer' }}
+                        >
+                            <Box flex="1">{produto.id}</Box>
+                            <Box w={"650px"} flex="2">{produto.nome}</Box>
+                            <Box flex="2">{produto.nome}</Box>
+                            <Box flex="2">{produto.descricao}</Box>
+                            <Box flex="2">{produto.sku}</Box>
+                            <Box flex="3">{produto.unidademedida}</Box>
+                            <Box flex="3">{produto.status}</Box>
+                            <Box flex="3">{produto.fornecedor_id}</Box>
+                            <Box flex="3">{produto.categoriaid}</Box>
+                            <Flex gap={2} flex="1">
+                                <Button
+                                    backgroundColor={"rgba(62, 43, 143, 1)"}
+                                    _hover={{ backgroundColor: "rgba(113, 100, 172, 1)" }}
+                                    color={"white"}
+                                    onClick={() => navigate(`/produtos/editar/${produto.id}`)}>
+                                    Editar
+                                </Button>
+                                <Button
+                                    backgroundColor={"rgba(141, 23, 23, 1)"}
+                                    _hover={{ backgroundColor: "rgba(167, 80, 80, 1)" }}
+                                    color={"white"}
+                                    onClick={() => produto.id && handleDell(produto.id)}>
+                                    Excluir
+                                </Button>
+                            </Flex>
+                        </Flex>
+                    ))}
+                </Box>
+            </Box>
         </Box>
     );
-}
+};
 
 export default ListarProduto;
--- frontend/src/pages/RoutesProduto/Services.ts
+++ frontend/src/pages/RoutesProduto/Services.ts
@@ -17,3 +17,37 @@ export const handlerProduto = async (produto: Produto) => {
         return
     }
 }
+
+// Função para listar produtos
+export const fetchProdutos = async () => {
+    const token = localStorage.getItem('token')
+    try {
+        const response = await axios.get(`${process.env.REACT_APP_NEXT_PUBLIC_API_URL}/produtos`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        return response;
+    } catch (error) {
+        console.error(error);
+        return
+    }
+}
+
+// Função para deletar produto
+export const deleteProduto = async (id: number) => {
+    const token = localStorage.getItem('token')
+    try {
+        const response = await axios.delete(`${process.env.REACT_APP_NEXT_PUBLIC_API_URL}/produtos/${id}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        return response;
+    } catch (error) {
+        console.error(error);
+        return
+    }
+}
